9421ac178ecde6287255f4fb30b2c3297bd972ba
Tatyana404 <kot.kate2020@gmail.com>
2025-03-26 10:56:14 +0100

    fix CORS

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,15 @@ const router = require("./routes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://tatyanadev-superheroes.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
